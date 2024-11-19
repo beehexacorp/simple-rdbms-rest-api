4900615163cf07d99e75b522dfc20d6a8fd92dac
nghbinh <64148429+binhhng@users.noreply.github.com>
2024-11-19 16:06:19 +0700

    update

--- TKSoutdoorsparts/Constants/RegexConstants.cs
+++ TKSoutdoorsparts/Constants/RegexConstants.cs
@@ -1,9 +1,11 @@
 namespace TKSoutdoorsparts.Constants
 {
-    public class RegexConstants
+    public static class RegexConstants
     {
-        public const string dangerousCommands = @"(?i)\b(DROP\s+(TABLE|INDEX|DATABASE)|ALTER\s+(TABLE|INDEX)|RENAME\s+INDEX)\b";
+        public const string dangerousCommands =
+            @"(?i)\b(DROP\s+(TABLE|INDEX|DATABASE)|ALTER\s+(TABLE|INDEX)|RENAME\s+INDEX)\b";
 
-        public const string embeddedString = @"(?i)(\b[\w.]+?\s*=\s*(?:ANY\([^()]+\)|'[^']*'|[^()\s]+)|\b[\w.]+\s+IS\s+NOT\s+NULL|\b[\w.]+\s+IS\s+NULL)";
+        public const string embeddedString =
+            @"(?i)(\b[\w.]+?\s*=\s*(?:ANY\([^()]+\)|'[^']*'|[^()\s]+)|\b[\w.]+\s+IS\s+NOT\s+NULL|\b[\w.]+\s+IS\s+NULL)";
     }
 }
